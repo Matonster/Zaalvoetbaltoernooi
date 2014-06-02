4b2e5b9d6350fa778540da857ec0bcd58ab90373
Toez <tommydehoon@gmail.com>
2014-06-02 20:20:57 +0200

    code
    added database and some methods

---
+++ Fifa_Dev_Edition/Fifa_Dev_Edition/FormMain.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Fifa_Dev_Edition
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+            lblCurrenttime.Text = DateTime.UtcNow.ToString();
+        }
+
+        private void btnLogOut_Click(object sender, EventArgs e)
+        {
+            Hide();
+        }
+
+        private void btnBetHistory_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnBets_Click(object sender, EventArgs e)
+        {
+            Predictions.Predict(txtGame.Text, int.Parse(txtTeam1.Text), int.Parse(txtTeam1.Text));
+        }
+    }
+}
