3eea61d1fdbbf3f31a6ca5dd5e5cbd4a5c987d50
Boaz-Frey <Boaz.Frey@gmail.com>
2014-06-03 12:45:44 +0200

    rollen

---
+++ Fifa_Dev_Edition/Fifa_Dev_Edition/FormMain.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Fifa_Dev_Edition
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+
+            lblCurrenttime.Text = DateTime.Now.ToString();
+
+            btnBets.Enabled = DateTime.Now < new DateTime(2014, 06, 12);
+        }
+
+        private void btnLogOut_Click(object sender, EventArgs e)
+        {
+            Hide();
+        }
+
+        private void btnBetHistory_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnBets_Click(object sender, EventArgs e)
+        {
+            Predictions.Predict(txtGame.Text, int.Parse(txtTeam1.Text), int.Parse(txtTeam1.Text));
+        }
+    }
+}
---
+++ Fifa_Dev_Edition/Fifa_Dev_Edition/Predictions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fifa_Dev_Edition
+{
+    class Predictions
+    {
+        public static void Predict(string match, int score1, int score2)
+        {
+            // Add user id, matchid, score1, score2 to tblPredictions
+<<<<<<< HEAD
+=======
+
+>>>>>>> 5b09a6a793d2f216e7252d0bb0ee525b49513cc3
+        }
+    }
+}
